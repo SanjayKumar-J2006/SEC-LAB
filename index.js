1cbf2e5dddac566fc2927e4574f2a706f7c55204
SanjayKumar-J2006 <sanjaykumar2006j@gmail.com>
2025-07-21 15:36:50 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-var w = 10;
-let name = 'js';
-const is = true;
-console.log(w+" "+name+" "+is);
-function add(a,b)
-{
-    console.log("this is a adding function");
-    console.log(a+b);
-}
-add(10,10);
-let x = parseInt(prompt("Enter 1 value: "));
-let y = parseInt(prompt("Enter 2 value: "));
-alert(x+y);
+let a=parseint(prompt ("enter the first value"));
+let b=parseint(prompt("enter the seond value"));
+
+alert(a+b);
